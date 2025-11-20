ae799263ba08352e95473379325b7771b1a5343c
Mikaela Sturk <mikaelasturk@gmail.com>
2025-11-20 16:27:39 +0100

    built folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import { ArticleCard } from "./Components/Cards/ArcticleCard"
 export const App = () => {
   return (
     <>
-      <h1>Portfolio</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, laborum! Maxime animi nostrum facilis distinctio neque labore consectetur beatae eum ipsum excepturi voluptatum, dicta repellendus incidunt fugiat, consequatur rem aperiam.</p>
+      <ArticleCard></ArticleCard>
     </>
   )
 }
---
+++ src/Components/cards/ArcticleCard.jsx
@@ -0,0 +1,9 @@
+import { Button } from "../Button/Button" 
+export const ArticleCard = () => {
+  return (
+    <>
+    <Button></Button>
+    <Button></Button>
+    </>
+  )
+}
